68bdc96306a543f7a5193a1eec0956f76f9ad55e
Ameer <ameerapex496@gmail.com>
2023-12-12 10:33:36 +0100

    added little to no html, added code - seeked help

--- src/script.js
+++ src/script.js
@@ -1,5 +1,31 @@
-//selecting our elements
-var cliderHandler = document.getElementsByClassName("clider-Handler")[0];
-//gelobal veribales
-// functions
-// eventlisnters
+// Selecting our elements
+var sliderHandler = document.getElementsByClassName("clider-Handler")[0];
+var sliderRange = document.getElementsByClassName("slider-Range")[0];
+var sliderNumber = document.getElementsByClassName("slider-Number")[0];
+// Global variables
+var isDragging = false;
+// Event listeners
+sliderHandler.addEventListener("mousedown", function (event) {
+    isDragging = true;
+    var handleMouseMove = function (e) {
+        if (isDragging) {
+            var mouseX = e.clientX;
+            var currentX = mouseX - sliderRange.getBoundingClientRect().left;
+            // Calculate the maximum X value considering the width of the slider handle
+            var maxX = sliderRange.offsetWidth - sliderHandler.offsetWidth;
+            // Ensure the slider handle stays within the slider range
+            var clampedX = Math.min(Math.max(currentX, 0), maxX);
+            // Calculate the percentage position based on the width of the slider range
+            var percentagePosition = (clampedX / maxX) * 100;
+            sliderNumber.textContent = "".concat(Math.floor(percentagePosition));
+            sliderHandler.style.left = "".concat(percentagePosition, "%");
+        }
+    };
+    var handleMouseUp = function () {
+        isDragging = false;
+        window.removeEventListener("mousemove", handleMouseMove);
+        window.removeEventListener("mouseup", handleMouseUp);
+    };
+    window.addEventListener("mousemove", handleMouseMove);
+    window.addEventListener("mouseup", handleMouseUp);
+});
--- src/script.ts
+++ src/script.ts
@@ -1,8 +1,40 @@
-//selecting our elements
-const cliderHandler = document.getElementsByClassName("clider-Handler")[0];
+// Selecting our elements
+const sliderHandler = document.getElementsByClassName("clider-Handler")[0];
+const sliderRange = document.getElementsByClassName("slider-Range")[0];
+const sliderNumber = document.getElementsByClassName("slider-Number")[0];
 
-//gelobal veribales
+// Global variables
+let isDragging = false;
 
-// functions
+// Event listeners
+sliderHandler.addEventListener("mousedown", (event) => {
+  isDragging = true;
 
-// eventlisnters
+  const handleMouseMove = (e) => {
+    if (isDragging) {
+      const mouseX = e.clientX;
+      const currentX = mouseX - sliderRange.getBoundingClientRect().left;
+
+      // Calculate the maximum X value considering the width of the slider handle
+      const maxX = sliderRange.offsetWidth - sliderHandler.offsetWidth;
+
+      // Ensure the slider handle stays within the slider range
+      const clampedX = Math.min(Math.max(currentX, 0), maxX);
+
+      // Calculate the percentage position based on the width of the slider range
+      const percentagePosition = (clampedX / maxX) * 100;
+
+      sliderNumber.textContent = `${Math.floor(percentagePosition)}`;
+      sliderHandler.style.left = `${percentagePosition}%`;
+    }
+  };
+
+  const handleMouseUp = () => {
+    isDragging = false;
+    window.removeEventListener("mousemove", handleMouseMove);
+    window.removeEventListener("mouseup", handleMouseUp);
+  };
+
+  window.addEventListener("mousemove", handleMouseMove);
+  window.addEventListener("mouseup", handleMouseUp);
+});
